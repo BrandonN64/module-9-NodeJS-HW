4b55ef6107ba555120e6c8f020b2ee7151926341
BrandonN64 <Brandon.nastri@gmail.com>
2022-12-18 15:01:19 -0500

    created index.html, imported bootstrap and set up basic webpage, started working on javascript

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+var searchButton = document.getElementById("searchButton")
+var userCity = document.querySelector('#cityName')
+var openWeatherApi = '5d21e5ba4232f7c4c356ba55ff9657a1'
 
 // TODO: Create an array of questions for user input
 const questions = [];
